c4cc96f33fd72cbb4ca99c50c2c62c39f612c362
prathamshanbhag99 <prathamshanbhag99@gmail.com>
2024-10-11 11:24:23 +0530

    Updated as on 11-10-2024

--- Oct2024/src/test/java/pac1/PersonMain.java
+++ Oct2024/src/test/java/pac1/PersonMain.java
@@ -1,68 +1,94 @@
 package pac1;
 
-import java.util.*;
- 
-public class PersonMain {
-	private String FirstName;
-	private String LastName;
-	private char Gender;
-	private long phoneNo;
-	
-	public long getPhoneNo() {
-		return phoneNo;
-	}
- 
-	public void setPhoneNo(long phoneNo) {
-		this.phoneNo = phoneNo;
-	}
- 
-	private void setFirstName(String FirstName) {
-		this.FirstName=FirstName;
-	}
-	
-	public String getFirstName() {
-		return FirstName;
-	}
-	
-	private void setLastName(String LastName) {
-		this.LastName=LastName;
-	}
-	
-	public String getLastName() {
-		return LastName;
-	}
-	
-	private void setGender(char Gender) {
-		this.Gender=Gender;
-	}
-	
-	public char getGender() {
-		return Gender;
-	}
-	
-	public void display(String FirstName,String LastName,char Gender,long Phone) {
-		System.out.print("FirstName:"+FirstName+'\n'+"LastName:"+LastName+'\n'+"Gender:"+Gender+'\n'+"Phone:"+Phone);
-	}
- 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Scanner sc=new Scanner(System.in);
-		PersonMain person=new PersonMain();
-		String fname=sc.nextLine();
-		String lname=sc.nextLine();
-		char g=sc.next().charAt(0);
-		long ph=sc.nextLong();
-		person.setFirstName("Ashwin");
-		String FirstName=person.getFirstName();
-		person.setLastName("Murugan");
-		String LastName=person.getLastName();
-		person.setGender('M');
-		char Gender=person.getGender();
-		person.setPhoneNo(ph);
-		long Phone=person.getPhoneNo();
-		person.display(FirstName,LastName,Gender,Phone);
-	}
- 
+import java.util.Scanner;
+
+enum Gender {
+    MALE, FEMALE;
 }
 
- 
+public class PersonMain {
+    private String firstName;
+    private String lastName;
+    private Gender gender;
+    private long phoneNo;
+
+    public long getPhoneNo() {
+        return phoneNo;
+    }
+
+    public void setPhoneNo(long phoneNo) {
+        this.phoneNo = phoneNo;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setGender(Gender gender) {
+        this.gender = gender;
+    }
+
+    public Gender getGender() {
+        return gender;
+    }
+
+    public void display() {
+        System.out.println("Person Details:");
+        System.out.println("First Name: " + getFirstName());
+        System.out.println("Last Name: " + getLastName());
+        System.out.println("Gender: " + getGender());
+        System.out.println("Phone: " + getPhoneNo());
+    }
+
+    public static void main(String[] args) {
+        // Input from user
+        Scanner sc = new Scanner(System.in);
+
+        // Creating person object
+        PersonMain person = new PersonMain();
+
+        // Taking inputs from the user
+        System.out.print("Enter First Name: ");
+        String fname = sc.nextLine();
+        person.setFirstName(fname);
+
+        System.out.print("Enter Last Name: ");
+        String lname = sc.nextLine();
+        person.setLastName(lname);
+
+        Gender gender = null;
+        while (gender == null) {
+            System.out.print("Enter Gender (M/F): ");
+            char g = sc.next().charAt(0);
+            if (g == 'M' || g == 'm') {
+                gender = Gender.MALE;
+            } 
+            else if (g == 'F' || g == 'f') {
+                gender = Gender.FEMALE;
+            } 
+            else {
+                System.out.println("Invalid input. Please enter 'M' for Male or 'F' for Female.");
+            }
+        }
+        person.setGender(gender);
+
+        System.out.print("Enter Phone Number: ");
+        long ph = sc.nextLong();
+        person.setPhoneNo(ph);
+
+        person.display();
+    }
+}
+
--- Oct2024/src/test/java/pac1/TC006_GetterAndSetter.java
+++ Oct2024/src/test/java/pac1/TC006_GetterAndSetter.java
@@ -1,35 +1,56 @@
 package pac1;
 
-
 public class TC006_GetterAndSetter {
-	String name;
-	int age;
+    // Fields
+    String name;
+    int age;
+    String phoneNumber; // New field for phone number
 
-	public String getName() {
-		return name;
-	}
+    // Getter and Setter for Name
+    public String getName() {
+        return name;
+    }
 
-	public void setName(String name) {
-		this.name = name;
-	}
+    public void setName(String name) {
+        this.name = name;
+    }
 
-	public int getAge() {
-		return age;
-	}
+    // Getter and Setter for Age
+    public int getAge() {
+        return age;
+    }
 
-	public void setAge(int age) {
-		this.age = age;
-	}
+    public void setAge(int age) {
+        this.age = age;
+    }
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
-		TC006_GetterAndSetter obj=new TC006_GetterAndSetter();
-		obj.setName("Leena");
-				System.out.println("Name : "+obj.getName());
-				obj.setAge(25);
-				System.out.println("Age :"+obj.getAge());
+    // Getter and Setter for Phone Number
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
 
-	}
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
 
+    // Method to display person details
+    public void displayDetails() {
+        System.out.println("Person Details:");
+        System.out.println("Name: " + getName());
+        System.out.println("Age: " + getAge());
+        System.out.println("Phone Number: " + getPhoneNumber());
+    }
+
+    public static void main(String[] args) {
+        // Creating object of TC006_GetterAndSetter class
+        TC006_GetterAndSetter person = new TC006_GetterAndSetter();
+        
+        // Setting values using setter methods
+        person.setName("Leena");
+        person.setAge(25);
+        person.setPhoneNumber("9876543210");  // Setting phone number
+        
+        // Displaying person details
+        person.displayDetails();
+    }
 }
